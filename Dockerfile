da365020f4f357932dac11cf4cde0909ca05ee33
Neil Berrington <nberrington@cloudian.com>
2020-06-03 19:17:26 +0000

    [cloudian] Use an alpine base for building the release image. Add a release.sh script that can put the built image into your KinD/microk8s cluster for testing prior to pushing to quay.io

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,21 @@
-FROM fedora:29
+# First stage - build the provisioner
+FROM golang:1.14-alpine3.11 as build
+RUN apk add --update build-base
 
-COPY ./bin/aws-s3-provisioner /usr/bin/
+# Copy source tree
+COPY vendor/ /app/vendor/
+COPY go.mod go.sum /app/
+COPY cmd/ /app/cmd/
 
-ENTRYPOINT ["/usr/bin/aws-s3-provisioner"]
+# Build
+WORKDIR /app
+RUN go build -a -o aws-s3-provisioner ./cmd
+
+
+# Final stage - distribution image
+FROM alpine:3.11
+
+COPY --from=build /app/aws-s3-provisioner /usr/local/bin/
+
+ENTRYPOINT ["/usr/local/bin/aws-s3-provisioner"]
 CMD ["-v=2", "-alsologtostderr"]
---
+++ scripts/release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+cd "$(dirname "$0")/.." || exit
+
+push=false
+if [ "$1" == "--push" ]; then
+  push=true
+  shift
+fi
+
+tag=${1:-quay.io/cloudian/hs-s3-provisioner:dev0.0.2}
+
+docker build -t "$tag" .
+
+# If we're running a kubernetes in docker cluster, load the image
+# into the cluster so we don't have to download it
+if [ -n "$(kubectl config get-contexts | grep "*" | grep "kind-kind")" ]; then
+    kind load docker-image "$tag"
+fi
+
+# If we're running a microk9s cluster, set a tag and push to the k8s registry
+if [ -n "$(kubectl config get-contexts | grep "*" | grep "microk8s")" ]; then
+  mk8s_tag="localhost:32000/$(basename "$tag")"
+  docker image tag "$tag" "$mk8s_tag"
+  # tolerate errors so we don't if the microk8s registry isn't enabled
+  set +e
+  docker push "$mk8s_tag"
+  set -e
+fi
+
+if $push; then
+  docker push "$tag"
+fi
